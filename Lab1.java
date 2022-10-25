5a66a2c78477880f7f3e33c6fea1c747daab73b8
DanielLongacre <danieljlongacre@gmail.com>
2022-10-25 13:12:34 -0400

    Lab1

---
+++ Lab1.java
@@ -0,0 +1,24 @@
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean boo = true;
+		byte maxByte = 127;
+		short minShort = -32_768;
+		int maxInt = 2_147_483_647;
+		long javaFlex = 3_000_000_000L;
+		float pie = 3.14159f;
+		double doubleYum = 3.14159 * 2;
+		
+		System.out.println("boo: " + boo);
+		System.out.println("maxByte: " + maxByte);
+		System.out.println("minShort: " + minShort);
+		System.out.println("maxInt: " + maxInt);
+		System.out.println("javaFlex: " + javaFlex);
+		System.out.println("pie: " + pie);
+		System.out.println("doubleYum: " + doubleYum);
+		
+	}
+
+}
